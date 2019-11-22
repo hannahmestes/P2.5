104fff2a9c59fb7aebca7e1f3eb0cf01677b01ec
Hannah <hannahmestes@gmail.com>
2019-11-21 21:49:49 -0600

    flasks with particles baby

---
+++ Assets/PourLiquid.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PourLiquid : MonoBehaviour
+{
+    public ParticleSystem particleEffect;
+    public GameObject interactableParent;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(interactableParent.transform.rotation.z > .69 || 
+        interactableParent.transform.rotation.z < -.69 ||
+        interactableParent.transform.rotation.x > .69 ||
+        interactableParent.transform.rotation.x < -.69){
+            particleEffect.Play(true);
+        }
+        else{
+            particleEffect.Stop(true);
+            //particleEffect.Clear(true);
+        }
+    }
+}
