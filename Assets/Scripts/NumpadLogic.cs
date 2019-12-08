3a5d89b3a80fbc014f492b9d9dbf2218dfd9720f
Tasnia Ashrafi Heya <tasnia.heya@gmail.com>
2019-12-07 23:19:36 -0600

    numpad logic

---
+++ Assets/Scripts/NumpadLogic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumpadLogic : MonoBehaviour
+{
+    public string winCode ="3862";
+    protected string currentCode = "1234";
+    public UnityEvent onWin;
+    public TMPro.TextMeshProUGUI display;
+    void Awake()
+    {
+        currentCode = display.text;
+    }
+
+    public void ButtonPressed( int val){
+        currentCode = currentCode.Substring(1) + val.ToString();
+        display.text = currentCode;
+
+        if ( currentCode == winCode){
+            Debug.Log("winner!!!!!");
+            if(onWin != null){
+                onWin.Invoke();
+            }
+        }
+    }
+
+   
+}
