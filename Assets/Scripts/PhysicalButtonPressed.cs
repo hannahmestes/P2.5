4a693d78815170ddc79c0cf236ef3521b041634d
Tasnia Ashrafi Heya <tasnia.heya@gmail.com>
2019-12-07 22:54:53 -0600

    added numpad

---
+++ Assets/Scripts/PhysicalButtonPressed.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicalButtonPressed : MonoBehaviour
+{
+	public UnityEvent onPressed;
+	protected bool lastState = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SetState(bool state){
+        if( state && !lastState && onPressed != null){
+            Debug.log("Button Pressed");
+            onPressed.Invoke();
+        }
+        lastState = state;
+    }
+}
